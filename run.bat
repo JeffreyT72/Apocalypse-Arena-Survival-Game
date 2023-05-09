e40af315401bc4d45488478abab3e79609f3820b
pprabhu2727 <prasadprabhu@hotmail.com>
2023-05-09 12:08:08 -0700

    potential fix

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 finalProject.MyGame 10.0.0.17 6010 UDP
+java -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 finalProject.MyGame 192.168.1.16 6010 UDP
